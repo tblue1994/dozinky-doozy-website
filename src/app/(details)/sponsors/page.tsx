215b49d2e9bce2dccc1d7fc90be280f970a2f429
Ashlyn Slawnyk <ashlyn.slawnyk@hudl.com>
2024-07-25 01:55:56 +0000

    Basic popover nav

---
+++ src/app/(details)/sponsors/page.tsx
@@ -0,0 +1,5 @@
+export default function Sponsors() {
+  return (
+    <div>Please support our sponsors.</div>
+  );
+}
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,13 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 import { RegisterLink } from "./RegisterLink";
+import clsx from "clsx";
+
+const aboutChildren = [
+  { href: "/routes", text: "Routes" },
+  { href: "/sponsors", text: "Sponsors" },
+  // TODO: rules and faq pages
+  // { href: "/rules", text: "Rules" },
+  // { href: "/faq", text: "FAQ" },
+];
+
+const teamChildren = [
+  { href: "/team", text: "Team" },
+  { href: "/contact", text: "Contact" },
+];
 
 export const NavBar = ({
   textColor = "black",
 }: {
   textColor?: "white" | "black";
 }) => {
-  const linkClassNames = `mx-2 text-${textColor} hover:underline`;
+  const baseLinkClassNames = `mx-2 text-${textColor}`
+  const linkClassNames = `${baseLinkClassNames} hover:underline`;
+  const popoverClassNames = clsx(
+    'hidden group-hover:block absolute right-0 bg-opacity-85 rounded p-1 text-right',{
+    'bg-sapphire-300': textColor === 'white',
+    'bg-sapphire-200': textColor === 'black',
+  });
+
   return (
     <nav className="flex shrink-0 items-center pt-1 px-4 w-full z-10">
       <Link href="\" className="mr-auto hover:opacity-85">
@@ -20,12 +41,31 @@ export const NavBar = ({
           className="my-[-16%]"
         />
       </Link>
-      <Link href="/routes" className={linkClassNames}>
-        Routes
-      </Link>
-      <Link href="/team" className={linkClassNames}>
-        Team
-      </Link>
+      <span className={`${baseLinkClassNames} group relative`}>
+        About &#9662;
+        <ul className={popoverClassNames}>
+          {aboutChildren.map(({ href, text}) => (
+            <li key={href}>
+              <Link href={href} className={linkClassNames}>
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </span>
+      <span className={`${baseLinkClassNames} group relative`}>
+        TEAM &#9662;
+        <ul className={popoverClassNames}>
+          {teamChildren.map(({ href, text}) => (
+            // TODO: different team nav header
+            <li key={href}>
+              <Link href={href} className={linkClassNames}>
+                {text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </span>
       <Link href="/lodging" className={linkClassNames}>
         Lodging
       </Link>
