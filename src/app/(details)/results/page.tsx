d8061a0933cd3508573e9275afad63a9419e0c77
Trevor Slawnyk <tblue1994@hotmail.com>
2025-09-28 18:21:52 -0500

    feat: add 2025 results

--- src/app/(details)/results/page.tsx
+++ src/app/(details)/results/page.tsx
@@ -6,8 +6,17 @@ const h2ClassNames = "text-2xl";
 export default function Results() {
   return (
     <div className="flex flex-col gap-6 w-fit mx-auto">
-      <h1 className={`${headerText.className} text-4xl`}>Fastest Known Time</h1>
-      <h2 className={h2ClassNames}>Steve Morin - 9 hours 8 minutes - 2024</h2>
+      <h1 className={`${headerText.className} text-4xl`}>Course Record</h1>
+      <h2 className={h2ClassNames}>
+        Stephano Porter - 8 hours 56 minutes - 2025
+      </h2>
+      <Link
+        href="/results-2025.pdf"
+        target="_blank"
+        className={`${h2ClassNames} underline hover:text-venetian-red-600`}
+      >
+        2025 Results
+      </Link>
       <Link
         href="/2024-results.pdf"
         target="_blank"
@@ -17,7 +26,15 @@ export default function Results() {
       </Link>
       <br />
       <h1 className={`${headerText.className} text-4xl`}>Photos</h1>
-      <h2 className={h2ClassNames}>Steve Morin - 9 hours 8 minutes - 2024</h2>
+      <h2 className={h2ClassNames}>2025</h2>
+      <Link
+        href="https://matttakespictures.zenfoliosite.com/dozinky-doozy-2025"
+        target="_blank"
+        className={`${h2ClassNames} underline hover:text-venetian-red-600`}
+      >
+        2025 - Matt Pearson Photography
+      </Link>
+      <h2 className={h2ClassNames}>2024</h2>
       <Link
         href="https://gallery.ericturner.photography/-dozinkydoozy/gallery"
         target="_blank"
